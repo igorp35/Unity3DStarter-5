795e7594757fd59ad8959ddb5de3d0719309a078
igor35 <igorp35@gmail.com>
2016-09-18 10:38:22 +0300

    UnityStarter5

---
+++ Assets/Scripts/MenuHelper.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuHelper : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void ClickButton(){
+	
+		Debug.Log ("ClickButton()");
+	}
+
+	public void ToggleButton(bool isOn){
+
+		Debug.Log ("TaggelButton(bool isOn)" + isOn);
+	}
+}
+
---
+++ Assets/Scripts/ToggleHelper.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class ToggleHelper : MonoBehaviour {
+	 
+	InputField field;
+
+	// Use this for initialization
+	void Start () {
+
+		field = GameObject.FindObjectOfType<InputField> ();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnChenged(bool isOn){
+	
+		if(isOn){
+
+			field.text = gameObject.name;
+		}
+	}
+}
